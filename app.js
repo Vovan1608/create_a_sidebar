307e11b27c38dea8f365bb45505bad6b042ca6ef
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-13 19:01:16 +0300

    added addEventListener for toggleBtn, closeBtn

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const sidebar = document.querySelector(".sidebar");
 const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 
+toggleBtn.addEventListener("click", () => {
+	sidebar.classList.add("show-sidebar");
+});
+
+closeBtn.addEventListener("click", () => {
+	sidebar.classList.remove("show-sidebar");
+});
