a87fcafac718e273bb059d4053a8dbf744dfd08d
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-13 18:56:13 +0300

    added variables sidebar, toggleBtn, closeBtn

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const sidebar = document.querySelector(".sidebar");
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+
